018e89f2e36da8c0b2330d181651e45c63be5373
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-12-16 21:32:11 +0200

    add to contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import contacts from "./contacts.json";
+// import contacts from "./contacts.json";
 import { useState } from "react";
 import "./App.css";
 
-// console.log(contacts);
-
 function App() {
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
   const [filter, setFilter] = useState("");
 
   const normalizedFilter = filter.toLowerCase().trim();
@@ -19,7 +23,7 @@ function App() {
   return (
     <div className="main">
       <h1 className="mainText">Phonebook</h1>
-      <ContactForm />
+      <ContactForm  onAddContact={setContacts} />
       <SearchBox value={filter} onChange={setFilter} />
       <ContactList contacts={filteredContacts} />
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,39 @@
 import { Formik, Form, Field } from "formik";
-import css from './ContactForm.module.css'
+import css from "./ContactForm.module.css";
+import { nanoid } from "nanoid";
 
-function ContactForm() {
+function ContactForm({ onAddContact }) {
   return (
     <Formik
-      initialValues={{ name: "", number: "" }}
-      onSubmit={(values, actions) => {console.log(values); actions.resetForm();}}
+      initialValues={{ id: nanoid(), name: "", number: "" }}
+      onSubmit={(values, actions) => {
+        onAddContact((prev) => [...prev, values]);
+        actions.resetForm();
+      }}
     >
       <Form className={css.form}>
         <label htmlFor="name" className={css.label}>
           Name
         </label>
-        <Field id="name" name="name" type="text" className={css.field} />
+        <Field
+          id="name"
+          name="name"
+          type="text"
+          autoComplete="name"
+          className={css.field}
+        />
 
         <label htmlFor="number" className={css.label}>
           Number
         </label>
-        <Field id="number" name="number" type="tel" className={css.field} />
+        <Field
+          id="number"
+          name="number"
+          type="tel"
+          autoComplete="tel"
+          inputMode="tel"
+          className={css.field}
+        />
 
         <button type="submit" className={css.formBtn}>
           Add contact
@@ -26,4 +43,4 @@ function ContactForm() {
   );
 }
 
-export default ContactForm
+export default ContactForm;
