a172918c2c8a2602a651605c401c12a4fe8977c4
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-12-16 20:15:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import contacts from "./contacts.json";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 // console.log(contacts);
 
 function App() {
   const [filter, setFilter] = useState("");
-  const filteredContacts = useMemo(() => {
-    const normalized = filter.toLowerCase().trim();
-    return contacts.filter((c) => c.name.toLowerCase().includes(normalized));
-  }, [filter]);
+
+  const normalizedFilter = filter.toLowerCase().trim();
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
 
   return (
     <div className="main">
