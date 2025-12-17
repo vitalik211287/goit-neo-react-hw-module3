4176d8fa3bf207c60995848f86528fc7fa6429d0
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-12-17 21:54:39 +0200

    useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,63 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 // import contacts from "./contacts.json";
+import useLocalStorage from "./hooks/useLocalStorage";
 import { useState } from "react";
 import "./App.css";
 
+// function App() {
+//   const [contacts, setContacts] = useState([
+//     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//   ]);
+//   const [filter, setFilter] = useState("");
+// console.log(contacts);
+//   const normalizedFilter = filter.toLowerCase().trim();
+
+//   const filteredContacts = contacts.filter((contact) =>
+//     contact.name.toLowerCase().includes(normalizedFilter)
+//   );
+
+//   return (
+//     <div className="main">
+//       <h1 className="mainText">Phonebook</h1>
+//       <ContactForm onAddContact={setContacts} />
+//       <SearchBox value={filter} onChange={setFilter} />
+//       <ContactList contacts={filteredContacts} onAddContact={setContacts} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// import ContactForm from "./components/ContactForm/ContactForm";
+// import SearchBox from "./components/SearchBox/SearchBox";
+// import ContactList from "./components/ContactList/ContactList";
+// import useLocalStorage from "./hooks/useLocalStorage";
+// import { useState } from "react";
+// import "./App.css";
+
+
+const INITIAL_CONTACTS = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useLocalStorage("contacts", INITIAL_CONTACTS);
+
   const [filter, setFilter] = useState("");
-console.log(contacts);
+
   const normalizedFilter = filter.toLowerCase().trim();
 
-  const filteredContacts = contacts.filter((contact) =>
+  const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
 
